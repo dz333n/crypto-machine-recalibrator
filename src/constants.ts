6797954f2487c38220888c9981f7140ae5dc8874
Yaroslav Kibysh <yanet.prod@gmail.com>
2024-05-12 23:06:39 +0300

    Remove BNB from calibration

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,5 @@ export const symbolsWhitelist: WhitelistedSymbol[] = [
   { sourceName: "BTCUSDT_4h_futures", cryptoMachineSymbol: "BTC/USDT" },
   { sourceName: "ADAUSDT_4h_futures", cryptoMachineSymbol: "ADA/USDT" },
   { sourceName: "SOLUSDT_4h_futures", cryptoMachineSymbol: "SOL/USDT" },
-  { sourceName: "BNBUSDT_4h_futures", cryptoMachineSymbol: "BNB/USDT" },
+  // { sourceName: "BNBUSDT_4h_futures", cryptoMachineSymbol: "BNB/USDT" },
 ];
