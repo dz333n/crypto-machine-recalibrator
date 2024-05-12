7f6674508c7696f939a12a27ea8c225d259c89ae
Yaroslav Kibysh <yanet.prod@gmail.com>
2024-05-12 19:54:43 +0300

    Update assets

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,7 @@ import WhitelistedSymbol from "./whitelisted-symbol";
 
 export const symbolsWhitelist: WhitelistedSymbol[] = [
   { sourceName: "BTCUSDT_4h_futures", cryptoMachineSymbol: "BTC/USDT" },
-  { sourceName: "ETHUSDT_4h_futures", cryptoMachineSymbol: "ETH/USDT" },
+  { sourceName: "ADAUSDT_4h_futures", cryptoMachineSymbol: "ADA/USDT" },
+  { sourceName: "SOLUSDT_4h_futures", cryptoMachineSymbol: "SOL/USDT" },
   { sourceName: "BNBUSDT_4h_futures", cryptoMachineSymbol: "BNB/USDT" },
 ];
